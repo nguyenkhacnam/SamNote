360a9af6866b7981a87f42a6465bda09686f56cb
toantran <boynhangheo2002hd@gmail.com>
2023-08-31 16:22:44 +0700

    navigation

---
+++ app/add-note/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+    return <div>add-note</div>;
+};
+
+export default page;
---
+++ app/user-profile/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+    return <div>user-profile</div>;
+};
+
+export default page;
