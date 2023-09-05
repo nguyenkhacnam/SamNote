8be97e0f3010c3ed1082ecb4114932c91a3d3ee7
QTruonggg <quangtruong102000@gmail.com>
2023-09-05 09:59:51 +0700

    .

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -1,9 +1,12 @@
+import Header from "@/components/Header";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-    return <div>user-profile</div>;
+    return <div className="h-screen md:h-full w-full md:px-[35px] md:py-[15px] lg:px-[95px] lg:py-[20px]">
+        <Header/>
+    </div>;
 };
 
 export default page;
