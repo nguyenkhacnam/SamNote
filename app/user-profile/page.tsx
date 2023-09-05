a14c9899fc6d08a8133a6d6000f8115aa0f18f17
QTruonggg <quangtruong102000@gmail.com>
2023-09-05 17:04:13 +0700

    edit profile

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -49,25 +49,40 @@ const page: FC<pageProps> = ({}) => {
                         </ul>
                     </div>
                 </div>
-                <div className="col-span-3 bg-gray-300 p-4 ml-4 rounded-[30px]">
-              
-                    <div className="tab-content">
-                        <div id="edit-profile" className={`container tab-pane ${activeTab === "edit-profile" ? "active" : "hidden"}`}><br />
-                            <h3>Profile</h3>
+                <div className="col-span-3 bg-gray-300 p-12 ml-12 rounded-[30px]">
+                    <div className="tab-content h-[100%]">
+                        <div id="edit-profile" className={`container tab-pane ${activeTab === "edit-profile" ? "active h-[100%]" : "hidden"}`}>
+                            <div className="h-[100%] px-8">
+                                <form action="" className="grid h-[100%]">
+                                    <div className="grid content-center">
+                                        <label htmlFor="" className="pt-4 font-semibold text-[20px]">Name</label>
+                                        <input type="text" className="w-[60%] h-[100%] m-[10px] text-[20px] outline-none rounded-xl pl-4" />
+                                    </div>
+                                    <label htmlFor="" className="font-semibold text-[20px]">Avatar</label>
+
+                                    <label htmlFor="" className="font-semibold text-[20px]">Background</label>
+
+                                    <div className="text-right font-semibold text-[20px]">
+                                        <button className="bg-white p-4 mr-12">Cancel</button>
+
+                                        <button className="bg-white p-4">Save</button>                                          
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div id="setting" className={`container tab-pane ${activeTab === "setting" ? "active" : "hidden"}`}><br />
+                        <div id="setting" className={`container tab-pane ${activeTab === "setting" ? "active h-[100%]" : "hidden"}`}>
                             <h3>Setting</h3>
                         </div>
-                        <div id="language" className={`container tab-pane ${activeTab === "language" ? "active" : "hidden"}`}><br />
+                        <div id="language" className={`container tab-pane ${activeTab === "language" ? "active h-[100%]" : "hidden"}`}>
                             <h3>Language</h3>
                         </div>
-                        <div id="dark-mode" className={`container tab-pane ${activeTab === "dark-mode" ? "active" : "hidden"}`}><br />
+                        <div id="dark-mode" className={`container tab-pane ${activeTab === "dark-mode" ? "active h-[100%]" : "hidden"}`}>
                             <h3>Dark Mode</h3>
                         </div>
-                        <div id="log-out" className={`container tab-pane ${activeTab === "log-out" ? "active" : "hidden"}`}><br />
+                        <div id="log-out" className={`container tab-pane ${activeTab === "log-out" ? "active h-[100%]" : "hidden"}`}>
                             <h3>Log Out</h3>
                         </div>
-                        <div id="help-about" className={`container tab-pane ${activeTab === "help-about" ? "active" : "hidden"}`}><br />
+                        <div id="help-about" className={`container tab-pane ${activeTab === "help-about" ? "active" : "hidden"}`}>
                             <h3>Help & About</h3>
                         </div>
                     </div>
