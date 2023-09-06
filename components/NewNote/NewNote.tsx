7dacd1c1884437741c60a66f04aff32fbfad2c1a
nguyenkhacnam <nguyenkhacnam06052001@gmail.com>
2023-09-06 17:29:25 +0700

    UI note

--- components/NewNote/NewNote.tsx
+++ components/NewNote/NewNote.tsx
@@ -59,16 +59,23 @@ const NewNote = () => {
   };
 
   return (
-    <div className='flex justify-center'>
-      <div className=' flex flex-col justify-between min-w-[1368px] max-w-[1000px] min-h-[587px] rounded-[20px]' style={{ backgroundColor: currentColor }}>
-        <div className='relative pt-6 px-[68px] '>
-          <div className='absolute right-[68px] w-[35px] h-[35px] bg-white flex justify-center items-center rounded-full'>
-            <BsPin className='text-[28px] cursor-pointer' />
+    <div className='xl:flex xl:justify-center 
+    flex justify-center'>
+      <div className=' xl:flex xl:flex-col xl:justify-between xl:min-w-[1368px] xl:max-w-[1000px] xl:min-h-[587px] xl:rounded-[20px]
+      flex flex-col justify-between min-w-[382px] max-w-[639px] min-h-[587px] rounded-[20px]' style={{ backgroundColor: currentColor }}>
+        <div className=' xl:relative xl:pt-6 xl:px-[68px] 
+        relative pt-6 px-[68px] '>
+          <div className=' xl:absolute xl:right-[68px] xl:w-[35px] xl:h-[35px] xl:bg-white xl:flex xl:justify-center xl:items-center xl:rounded-full
+          absolute right-[68px] w-[35px] h-[35px] bg-white flex justify-center items-center rounded-full'>
+            <BsPin className=' xl:text-[28px] xl:cursor-pointer
+            text-[28px] cursor-pointer' />
           </div>
-          <div className='flex justify-center'>
+          <div className=' xl:flex xl:justify-center
+          flex justify-center'>
             <h2 className={`text-2xl font-semibold ${titleTextColor}`}>Create Note</h2>
           </div>
-          <div className='flex flex-col'>
+          <div className=' xl:flex xl:flex-col
+          flex flex-col'>
             <label
               htmlFor="inputTitleField"
               className={`text-2xl font-semibold cursor-pointer ${titleTextColor}`}
@@ -79,7 +86,8 @@ const NewNote = () => {
             <div>
               <textarea name="textarea"
                 id="inputTitleField"
-                className='title text-xl'
+                className=' xl:title xl:text-xl
+                title text-xl'
                 style={{ backgroundColor: currentColor }}
                 ref={titleRef}
               >
@@ -87,7 +95,8 @@ const NewNote = () => {
             </div>
             {/* <div id='container-bubble'></div> */}
           </div>
-          <div className='flex flex-col'>
+          <div className=' xl:flex xl:flex-col
+          flex flex-col'>
           <label
               htmlFor="inputContentField"
               className={`text-2xl font-semibold cursor-pointer ${titleTextColor}`}
@@ -98,7 +107,8 @@ const NewNote = () => {
             <div>
               <textarea name="textarea"
                 id='inputContentField'
-                className='content text-xl'
+                className=' xl:content xl:text-xl
+                content text-xl'
                 style={{ backgroundColor: currentColor }}
                 ref={contentRef}
               >
@@ -106,10 +116,13 @@ const NewNote = () => {
             </div>
             <div className={`self-end text-sm font-normal ${titleTextColor}`}>Đã chỉnh sửa hôm 19/07/2023</div>
             {/* <div className='container' ref={wrapperRefSnow}></div> */}
+            {/* <div className='container' ref={wrapperRefSnow}></div> */}
           </div>
         </div>
-        <div className='flex justify-between pl-[44px] pr-[54px] pt-[33px] pb-[66px]'>
-          <div className='flex gap-[30px] items-center'>
+        <div className=' xl:flex xl:justify-between xl:pl-[44px] xl:pr-[54px] xl:pt-[33px] xl:pb-[66px] xl:sticky
+        flex justify-between pl-[44px] pr-[54px] pt-[33px] pb-[66px]'>
+          <div className='xl:flex xl:gap-[30px] xl:items-center
+          flex gap-[30px] items-center'>
             {
               colors?.map((color: string, index: number) => (
                 <ColorItem
@@ -120,7 +133,8 @@ const NewNote = () => {
               ))
             }
           </div>
-          <div className='flex items-center gap-[30px] text-[32px]'>
+          <div className=' xl:flex xl:items-center xl:gap-[30px] xl:text-[32px] xl:sticky xl:bg-transparent
+          flex items-center gap-[30px] text-[32px] absolute top-[140px] bg-slate-500'>
             <RiFontSize className={`icon-note ${titleTextColor}`} />
             <BsListUl className={`icon-note ${titleTextColor}`} />
             <PiImageSquare className={`icon-note ${titleTextColor}`} />
@@ -131,7 +145,8 @@ const NewNote = () => {
             <SlOptions className={`icon-note ${titleTextColor}`} />
           </div>
           <div>
-            <button className='w-[114px] h-[50px] bg-[#FFFFFF] text-[24px] font-semibold rounded-[30px]'>Done</button>
+            <button className=' xl:w-[114px] xl:h-[50px] xl:bg-[#FFFFFF] xl:text-[24px] xl:font-semibold xl:rounded-[30px]
+            w-[114px] h-[50px] bg-[#FFFFFF] text-[24px] font-semibold rounded-[30px]'>Done</button>
           </div>
         </div>
       </div>
